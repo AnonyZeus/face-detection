9bc93a608631e8b6e62b6139f11ef966c46b74be
SonLT <son.lt@vinayamato.com>
2019-05-27 15:17:33 +0700

    Add logic for faced

--- auto_sync.sh
+++ auto_sync.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-cd /home/ubuntu/face-detector/
+cd /home/ubuntu/face-detection/
 python auto_sync.py -p /home/ubuntu/aim-cloud/application/webroot/files/user_images_list.txt -i ubuntu@54.238.198.114 -d /home/ubuntu/face-detection/dataset -f true
+cd /home/ubuntu/face-detection && ./extract_faced.sh
+cd /home/ubuntu/face-detection && ./train_faced.sh
 kill -9 $(pgrep -f main.py)
 sh start_server.sh
