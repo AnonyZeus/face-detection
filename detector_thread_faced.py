ec4145df2eb6f4f9514347b8790befcee4934b7b
SonLT <son.lt@vinayamato.com>
2019-06-25 10:48:16 +0700

    Fix bug for realtime detection

--- detector_thread_faced.py
+++ detector_thread_faced.py
@@ -285,7 +285,6 @@ class Camera(object):
         start_index = 1
         while(True):
             img_path = f'/mnt/zoneminder/events/{monitor_id}/{event_date}/{event_id}/{start_index:05}-capture.jpg'
-            start_index += 1
             try:
                 print(f'[INFO] parsing {img_path}...')
                 ret, frame = cv2.imread(img_path)
@@ -295,11 +294,13 @@ class Camera(object):
                 for detect_id in detect_list:
                     if detect_id not in result_list:
                         result_list.append(detect_id)
-            except:
+            except Exception as e:
+                print(e)
                 print(
                     f'[INFO] failed to parsing frame {start_index} for event {event_id}...')
                 break
             finally:
+                start_index += 1
                 time.sleep(0.02)
         print('[INFO] finish video detection...')
         response_data['detection'] = result_list
